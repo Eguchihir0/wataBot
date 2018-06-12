88a2973cfd6034dbccb888296903134a0eb356bc
takahiro.eguchi <takahiro.eguchi@rakus.co.jp>
2018-06-12 23:32:45 +0900

    test

---
+++ wataBot/src/main/java/com/line/WataBotApplication.java
@@ -0,0 +1,12 @@
+package com.line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WataBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WataBotApplication.class, args);
+	}
+}
